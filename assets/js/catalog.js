bca645218458af76e90d9fbc8f829257319b9608
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2024-11-11 09:52:26 +0100

    l ajoute de fonction de checkbox

---
+++ assets/js/catalog.js
@@ -0,0 +1,8 @@
+function deselectOtherCheckboxes(selectedCheckbox) {
+    const checkboxes = document.querySelectorAll('input[name="marque"]');
+        checkboxes.forEach(checkbox => {
+      if (checkbox !== selectedCheckbox) {
+        checkbox.checked = false;
+      }
+    });
+  }
