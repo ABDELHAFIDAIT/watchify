b747d7f3969800fc3665835024b7faa613d72fd4
abdelkarim moussi <abdelkarimmoussi73@gmail.com>
2024-11-14 11:48:00 +0100

    mergeing with catalogue

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,21 +1,19 @@
 const { log } = require("console");
 
 // *************************************************************** Event handlers ********************************
-let gridProduit = document.getElementById('produit');
-let btnNext=document.getElementById('cata_btn_next');
-const productContainer = document.getElementById('produits_container');
-let min=0
-let max=8
+let gridProduit = document.getElementById("produit");
+let btnNext = document.getElementById("cata_btn_next");
+const productContainer = document.getElementById("produits_container");
+let min = 0;
+let max = 8;
 
-let paraGraph=document.getElementById('paragrapher_prix')
-let ImageCata=document.getElementById('image-principale')
-let i=0;
-
-
+let paraGraph = document.getElementById("paragrapher_prix");
+let ImageCata = document.getElementById("image-principale");
+let i = 0;
 
 function deselectOtherCheckboxes(selectedCheckbox) {
   const checkboxes = document.querySelectorAll('input[name="marque"]');
-  checkboxes.forEach(checkbox => {
+  checkboxes.forEach((checkbox) => {
     if (checkbox !== selectedCheckbox) {
       checkbox.checked = false;
     }
@@ -28,22 +26,28 @@ function deselectOtherCheckboxes(selectedCheckbox) {
 // .then(res => res.json())
 //   .then(data => {
 //     console.log(data);
-    
+
 //   productContainer.innerHTML=""
 //     const limitedProducts = data.slice(min+8, max+8);
 //     dataProduct.push(...data);
 //     console.log(limitedProducts);
-//     displayProducts(limitedProducts); 
+//     displayProducts(limitedProducts);
 
 //   });
 // }
 
 function displayProducts(products) {
-  productContainer.innerHTML = '';
-  products.forEach(product => {
-    const productCard = document.createElement('div');
-  
-    productCard.classList.add('product-card', 'p-4', 'shadow-md', 'rounded-md', 'text-center');
+  productContainer.innerHTML = "";
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+
+    productCard.classList.add(
+      "product-card",
+      "p-4",
+      "shadow-md",
+      "rounded-md",
+      "text-center"
+    );
     productCard.innerHTML = `
     <a href="details.html?${product.id}" class="w-full block">
       <div href="details.html?id=${product.id}" class="rounded-lg shadow-md p-4 h-[400px] flex flex-col">
@@ -62,30 +66,21 @@ function displayProducts(products) {
     productContainer.appendChild(productCard);
   });
 }
-btnNext.addEventListener('click', btnNextClike);
-const dataProduct = []; 
+btnNext.addEventListener("click", btnNextClike);
+const dataProduct = [];
 
-
-fetch('http://localhost:3000/products')
-.then(res => res.json())
-  .then(data => {
-    const limitedProducts = data.slice(min,max);
+fetch("http://localhost:3000/products")
+  .then((res) => res.json())
+  .then((data) => {
+    const limitedProducts = data.slice(min, max);
     dataProduct.push(...data);
-    displayProducts(limitedProducts); 
+    displayProducts(limitedProducts);
   });
 
-
-
-setInterval(function() {
+setInterval(function () {
   console.log(dataProduct);
-//   paraGraph.textContent=`${dataProduct[i].price}`
-//  ImageCata.src=`${dataProduct[i].images[0]}`
- 
- 
- i++;
- }, 1000);
+  //   paraGraph.textContent=`${dataProduct[i].price}`
+  //  ImageCata.src=`${dataProduct[i].images[0]}`
 
-
-
-
-
+  i++;
+}, 1000);
