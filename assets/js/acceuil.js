06857df79d908f5c2ab856c32339f841975505c9
Sahnoun122 <khadijashnoun70@gmail.com>
2024-11-16 14:48:49 +0100

    la modification sura la partie js

--- assets/js/acceuil.js
+++ assets/js/acceuil.js
@@ -10,16 +10,17 @@ document.addEventListener('DOMContentLoaded' ,function(){
         products.innerHTML  += `
 
                 <div class="product">
-                    <div class="flex flex-col items-center max-w-[250px] p-5 bg-slate-100 rounded-md gap-2">
-                        <img class="w-[100%]" src="${response[i].images[0]}" alt="produit" style="height:200px;width:200px;object-fit : fill">
-                        <h4 id="product-name" class="text-[1rem] font-semibold capitalize">${response[i].name}</h4>
-                        <h5 class="text-[0.9rem] font-semibold">$<span id="d-product-price">${response[i].price}</span></h5>
-                        <button type="button" id="d-add-to-cart_btn"
-                            class="bg-blue-600 px-4 py-1 text-white rounded-md text-center hover:bg-[#183876] transition-colors ease-in-out">
-                            Ajouter Au Panier
-                        </button>
-                    </div>
-                </div>
+                 <div class="flex flex-col items-center max-w-xs md:max-w-sm lg:max-w-md p-5 bg-slate-100 rounded-md gap-2">
+                  <img class="w-full h-48 md:h-56 lg:h-64 object-fill" src="${response[i].images[0]}" alt="produit">
+                    <h4 id="product-name" class="text-base md:text-lg lg:text-xl font-semibold capitalize">${response[i].name}</h4>
+                      <h5 class="text-sm md:text-base lg:text-lg font-semibold">$<span id="d-product-price">${response[i].price}</span></h5>
+                         <button type="button" id="d-add-to-cart_btn"
+                           class="bg-blue-600 px-4 py-1 text-white rounded-md text-center hover:bg-[#183876] transition-colors ease-in-out">
+                          Ajouter Au Panier
+                           </button>
+                   </div>
+               </div>
+
     `
     }
     
@@ -28,10 +29,6 @@ document.addEventListener('DOMContentLoaded' ,function(){
     fetchproducts('http://localhost:3000/produits');
 
 })
-
-
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
     let productsContainer = document.querySelector('.products6');
