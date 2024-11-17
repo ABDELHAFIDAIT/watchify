e554497bb285a3face94dc294dc7e17ad91df96f
abdelkarim moussi <abdelkarimmoussi73@gmail.com>
2024-11-17 01:40:31 +0100

    alignement des sectios des apage, mise a jour des images, reliant les produit avec la page details

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -5,15 +5,13 @@ const btnNext = document.getElementById("cata_btn_next");
 const btnPrev = document.getElementById("cata_btn_prev");
 const checkboxes = document.querySelectorAll('input[name="marque"]');
 let min = 0;
-let max = 8; 
+let max = 8;
 let AllProduit = [];
 
 let productsToShow = [];
 
-
 function deselectOtherCheckboxes(selectedCheckbox) {
-  
-      checkboxes.forEach(checkbox => {
+  checkboxes.forEach((checkbox) => {
     if (checkbox !== selectedCheckbox) {
       checkbox.checked = false;
     }
@@ -21,28 +19,42 @@ function deselectOtherCheckboxes(selectedCheckbox) {
 }
 
 // Fetch product data
-fetch('http://localhost:3000/produits')
-  .then(res => res.json())
-  .then(data => {
+fetch("http://localhost:3000/produits")
+  .then((res) => res.json())
+  .then((data) => {
     AllProduit.push(...data);
     displayProducts(min, max);
   })
-  .catch(err => console.error('Error fetching products:', err));
+  .catch((err) => console.error("Error fetching products:", err));
 
 // Function to display products based on the range
 function displayProducts(min, max) {
-  productContainer.innerHTML = ''; // Clear the current products before appending new ones
+  productContainer.innerHTML = ""; // Clear the current products before appending new ones
 
   productsToShow = AllProduit.slice(min, max);
 
   productsToShow.forEach((product) => {
     const productCard = document.createElement("div");
-    productCard.classList.add("product-card", "p-4", "shadow-md", "rounded-md", "text-center",);
+    productCard.classList.add(
+      "product-card",
+      "max-w-[250px]",
+      "min-w-[200px]",
+      "p-5",
+      "shadow-md",
+      "rounded-md",
+      "text-center",
+      "hover:shadow-blue-600",
+      "flex",
+      "flex-col",
+      "gap-2",
+      "justify-between",
+      "mx-auto"
+    );
 
     productCard.innerHTML = `
       
         <img src="${product.images[0]}" alt="${product.name}" class="w-full h-40 object-contain rounded-md mb-2">
-        <a href="details.html?${product.id}"><h3 class="text-sm font-semibold text-gray-800 h-9 overflow-hidden">${product.name}</h3></a>
+        <a href="details.html?${product.id}"><h3 class="text-sm font-semibold text-gray-800 h-9 overflow-hidden hover:text-blue-600">${product.name}</h3></a>
         <p class="text-gray-600 text-xs h-fit overflow-hidden flex-grow">${product.short_description}</p>
         <p class="text-blue-600 font-bold h-[30px]">${product.price}</p>
         <div class="mt-auto">
@@ -51,7 +63,6 @@ function displayProducts(min, max) {
      
     `;
     productContainer.appendChild(productCard);
-  
   });
 }
 
@@ -94,25 +105,23 @@ const intervalId = setInterval(updateCarousel, 1000);
 
 function resetCarousel() {
   clearInterval(intervalId);
-  setInterval(updateCarousel, 1000); 
+  setInterval(updateCarousel, 1000);
 }
 
-const btnSort=document.getElementById("sort")
+const btnSort = document.getElementById("sort");
 btnSort.addEventListener("click", () => {
-switch(btnSort.value){
-  case "price":
-    AllProduit.sort((a, b) => b.price - a.price);
-    displayProducts(min, max);
-    break;
-  case "stock":
-    AllProduit.sort((a, b) => b.stock - a.stock);
-    displayProducts(min, max);
-    console.log("Sort stock");
-    break;
-  default:
-    displayProducts(min, max);
-    break;
-}
-  
+  switch (btnSort.value) {
+    case "price":
+      AllProduit.sort((a, b) => b.price - a.price);
+      displayProducts(min, max);
+      break;
+    case "stock":
+      AllProduit.sort((a, b) => b.stock - a.stock);
+      displayProducts(min, max);
+      console.log("Sort stock");
+      break;
+    default:
+      displayProducts(min, max);
+      break;
+  }
 });
-
--- assets/js/details.js
+++ assets/js/details.js
@@ -23,10 +23,10 @@ function show(id) {
 }
 var full_url = document.URL;
 var stuff = full_url.split("?");
-var id = stuff[1];
+var id = stuff[stuff.length - 1] - 1;
 // let id = 10;
 
-fetch(`http://localhost:3000/produits/${id}`)
+fetch("http://localhost:3000/produits")
   .then((responce) => responce.json())
   .then((data) => {
     // data.map((data) => {
@@ -35,25 +35,26 @@ fetch(`http://localhost:3000/produits/${id}`)
     // console.log(data);
 
     //display product details
-    // for (let i = 0; i < data.length; i++) {
-      // if (i == id) {
-        productGalerie.innerHTML = `<div class="mb-5 flex justify-center" id="d-image-container">
-        <img id=${id} src="${data[id].images[0]}" alt="product Image"
+    for (let i = 0; i < data.length; i++) {
+      if (i == id) {
+        productGalerie.innerHTML = `
+    <div class="mb-5 flex justify-center" id="d-image-container">
+        <img id=${id} class="md:w-[50%] md:h-[450px] object-cover" src="${data[id].images[0]}" alt="product Image"
             style="width: 100%; max-width: 450px;">
     </div>
     <div class="flex gap-2 justify-center" id="d-image-variants">
         <div
-            class="d-variant bg-blue-600 rounded-md p-1 cursor-pointer max-w-[100px] max-h-[109px] my-auto">
-            <img class = "rounded-md"  src="${data[id].images[0]}" alt="product Image"
+            class="d-variant bg-blue-600 rounded-md p-1 cursor-pointer w-[100px] h-[109px] my-auto">
+            <img class = "rounded-md w-[100%] h-[100%]"  src="${data[id].images[0]}" alt="product Image"
                 onclick="switchImages(this)">
         </div>
         <div
-            class="d-variant bg-blue-600 rounded-md p-1 cursor-pointer max-w-[100px] max-h-[109px] my-auto">
-            <img class = "rounded-md" src="${data[id].images[1]}" alt="product Image" onclick="switchImages(this)">
+            class="d-variant bg-blue-600 rounded-md p-1 cursor-pointer w-[100px] h-[109px] my-auto">
+            <img class = "rounded-md w-[100%] h-[100%]" src="${data[id].images[1]}" alt="product Image" onclick="switchImages(this)">
         </div>
         <div
-            class="d-variant bg-blue-600 rounded-md p-1 cursor-pointer max-w-[100px] max-h-[109px] my-auto">
-            <img class = "rounded-md"  src="${data[id].images[2]}" alt="product Image" onclick="switchImages(this)">
+            class="d-variant bg-blue-600 rounded-md p-1 cursor-pointer w-[100px] h-[109px] my-auto">
+            <img class = "rounded-md w-[100%] h-[100%]"  src="${data[id].images[2]}" alt="product Image" onclick="switchImages(this)">
         </div>
 
     </div>`;
@@ -64,15 +65,15 @@ fetch(`http://localhost:3000/produits/${id}`)
         productStatus.innerHTML = data[id].stock;
         productPrice.innerHTML = data[id].price;
         totalPrice.innerHTML = data[id].price;
-      // }
-    // }
+      }
+    }
 
     // console.log("data", data);
 
     //display popular products
 
     for (let i = 0; i < 3; i++) {
-      popularProducts.innerHTML += `<div class="flex flex-col items-center max-w-[250px] p-5 bg-slate-100 rounded-md gap-2">
+      popularProducts.innerHTML += `<div class="flex flex-col items-center justify-between max-w-[250px] p-5 bg-white shadow-md hover:shadow-blue-600 rounded-md gap-2">
                     <img class="w-[100%]" src="${data[i].images[0]}" alt="product Image" style = "max-height : 200px">
                     <a href = "?${data[i].id}"><h4 id="product-name" class="text-[1rem] text-center font-semibold capitalize hover:text-blue-600">${data[i].name}</h4></a>
                     <h5 class="text-[0.9rem] font-semibold">$<span id="d-product-price">${data[i].price}</span></h5>
@@ -120,9 +121,5 @@ let changeQuantity = () => {
     }
   });
 };
-
-//Fonction d'ajout au panier
-
-let ajouterAuPanier = (id) => {};
 
 changeQuantity();
